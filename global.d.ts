3617b61a5fbd6ec399e2abc51da2b0bee262ba80
Kolomiiets Dmytro <nookon@icloud.com>
2025-07-03 16:06:13 +0200

    fix

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,16 @@
+interface TelegramWebApp {
+    sendData: (data: string) => void;
+    onEvent: (eventName: string, callback: () => void) => void;
+    offEvent: (eventName: string, callback: () => void) => void;
+    initDataUnsafe?: {
+        user?: {
+            username?: string;
+        };
+    };
+}
+
 interface Window {
     Telegram?: {
-        WebApp: any;
+        WebApp: TelegramWebApp;
     };
 }
