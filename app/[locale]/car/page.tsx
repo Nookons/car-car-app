a167b49e4b5a3dcd2345b47c1fcd7773ed27aaf6
Kolomiiets Dmytro <nookon@icloud.com>
2025-08-21 17:11:26 +0200

    test

--- app/[locale]/car/page.tsx
+++ app/[locale]/car/page.tsx
@@ -1,10 +1,6 @@
 'use client';
-import {useUserStore} from "@/store/user/userStore";
 
 export default function Page() {
-    const user_data = useUserStore(state => state.user_data)
-
-    if (!user_data.id || user_data.id === 0) return null;
 
     return (
         <div className="p-2">
